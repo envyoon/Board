0efefe54bd86a00cb082ba9dd3bca783c4136b2b
envyoon <krafftdj@daum.net>
2023-02-12 17:08:44 +0900

    feat: Spring Boot Interceptor 적용
    HandlerInterceptor를 implements 한 뒤에
    Alt + Shift + S -> V를 눌러 원하는 메서드 선택 후 적용 하여
    
    시작점과 끝 점을 착는 preHandle과 postHandle을 사용하였음.

--- Board/src/main/java/board/interceptor/LoggerInterceptor.java
+++ Board/src/main/java/board/interceptor/LoggerInterceptor.java
@@ -1,16 +1,37 @@
-//package board.interceptor;
-//
+package board.interceptor;
+
 //import javax.servlet.http.HttpServletRequest;
 //import javax.servlet.http.HttpServletResponse;
-//
-//import lombok.extern.slf4j.Slf4j;
-//
-//import org.springframework.web.servlet.HandlerInterceptor;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//@Slf4j
-//public class LoggerInterceptor implements HandlerInterceptor{
-//
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class LoggerInterceptor implements HandlerInterceptor{
+
+	// Alt + Shift + S -> V 후 원하는 메서드 선택 후 적용
+	
+	@Override
+	public boolean preHandle(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response, Object handler) throws Exception {
+		
+		log.debug("======================================          START         ======================================");
+		log.debug(" Request URI \t:  " + request.getRequestURI());
+		
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(jakarta.servlet.http.HttpServletRequest request,	jakarta.servlet.http.HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		
+		
+		
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+		
+		log.debug("======================================           END          ======================================\n");
+	}
+
 //	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 //		log.debug("======================================          START         ======================================");
 //		log.debug(" Request URI \t:  " + request.getRequestURI());
@@ -20,4 +41,4 @@
 //	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 //		log.debug("======================================           END          ======================================\n");
 //	}
-//}
+}
