02ff0230c794350739195f5dfb3136b530d27405
envyoon <krafftdj@daum.net>
2023-02-17 23:51:00 +0900

    fix: LoggerInterceptor 수정
    implements 로 상속 받던걸 extends 로 수정
    
    @@@@@@@@@@@@@@@@@@@@@@@@@@@@    전    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
    // Alt + Shift + S -> V 후 원하는 메서드 선택 후 적용
    @Slf4j
    public class LoggerInterceptor implements HandlerInterceptor{
    
    	// Alt + Shift + S -> V 후 원하는 메서드 선택 후 적용
    
    	@Override
    	public boolean preHandle(HttpServletRequest request,
    HttpServletResponse response, Object handler) throws Exception {
    		log.debug("======================================          START
    ======================================");
    		log.debug(" Request URI \t:  " + request.getRequestURI());
    		return preHandle(request, response, handler);
    	}
    
    	@Override
    	public void postHandle(HttpServletRequest request, HttpServletResponse
    response, Object handler, ModelAndView modelAndView) throws Exception {
    		log.debug("======================================           END
    ======================================\n");
    	}
    
    }
    
    @@@@@@@@@@@@@@@@@@@@@@@@@@@@    후    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
    @Slf4j
    public class LoggerInterceptor extends HandlerInterceptorAdapter{
    
    	@Override
    	public boolean preHandle(HttpServletRequest request,
    HttpServletResponse response, Object handler) throws Exception {
    		log.debug("======================================          START
    ======================================");
    		log.debug(" Request URI \t:  " + request.getRequestURI());
    		return super.preHandle(request, response, handler);
    	}
    
    	@Override
    	public void postHandle(HttpServletRequest request, HttpServletResponse
    response, Object handler, ModelAndView modelAndView) throws Exception {
    		log.debug("======================================           END
    ======================================\n");
    	}
    }

--- Board/src/main/java/board/interceptor/LoggerInterceptor.java
+++ Board/src/main/java/board/interceptor/LoggerInterceptor.java
@@ -7,23 +7,20 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 @Slf4j
-public class LoggerInterceptor implements HandlerInterceptor{
+public class LoggerInterceptor extends HandlerInterceptorAdapter{
 
-	// Alt + Shift + S -> V 후 원하는 메서드 선택 후 적용
-	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		log.debug("======================================          START         ======================================");
 		log.debug(" Request URI \t:  " + request.getRequestURI());
-		return preHandle(request, response, handler);
+		return super.preHandle(request, response, handler);
 	}
 	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 		log.debug("======================================           END          ======================================\n");
 	}
-
-
 }
