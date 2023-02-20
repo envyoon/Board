638961e175bff15fddf33c7956bfdae1dd7db226
envyoon <krafftdj@daum.net>
2023-02-20 23:09:50 +0900

    feat: 엔티티 생성 및 Jsr310JpaConverters 설정

--- Board/src/main/java/board/BoardApplication.java
+++ Board/src/main/java/board/BoardApplication.java
@@ -2,9 +2,14 @@ package board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(
+		basePackageClasses = {Jsr310JpaConverters.class},
+		basePackages = {"board"})
 public class BoardApplication {
 
 	public static void main(String[] args) {
