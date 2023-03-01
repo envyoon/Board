75dce4448e0da87c67821780f662dacafff69e0f
envyoon <krafftdj@daum.net>
2023-03-01 22:06:46 +0900

    feat: swagger 설명 추가

--- Board/src/main/java/board/board/controller/RestBoardApiController.java
+++ Board/src/main/java/board/board/controller/RestBoardApiController.java
@@ -13,35 +13,43 @@ import org.springframework.web.bind.annotation.RestController;
 
 import board.board.dto.BoardDto;
 import board.board.service.BoardService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
+@Api(description ="게시판 REST API")
 @RestController
 public class RestBoardApiController {
 	
 	@Autowired
 	private BoardService boardService;
 	
+	@ApiOperation(value = "게시글 목록 조회")
 	@GetMapping(value="/api/board")
 	public List<BoardDto> openBoardList() throws Exception{
 		return boardService.selectBoardList();
 	}
 	
+	@ApiOperation(value = "게시글 작성")
 	@PostMapping(value="/api/board/write")
 	public void insertBoard(@RequestBody BoardDto board) throws Exception{
 		boardService.insertBoard(board);
 	}
 	
+	@ApiOperation(value = "게시글 상세 내용 조회")
 	@GetMapping(value="/api/board/{boardIdx}")
 	public BoardDto openBoardDetail(@PathVariable("boardIdx") int boardIdx) throws Exception{
 		
 		return boardService.selectBoardDetail(boardIdx);
 	}
 	
+	@ApiOperation(value = "게시글 상세 내용 수정")
 	@PutMapping(value="/api/board/{boardIdx}")
 	public String updateBoard(@RequestBody BoardDto board) throws Exception{
 		boardService.updateBoard(board);
 		return "redirect:/board";
 	}
 	
+	@ApiOperation(value = "게시글 삭제")
 	@DeleteMapping(value="/api/board/{boardIdx}")
 	public String deleteBoard(@PathVariable("boardIdx") int boardIdx) throws Exception{
 		boardService.deleteBoard(boardIdx);
